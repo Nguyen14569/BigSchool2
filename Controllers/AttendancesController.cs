82d2f3bafca001795cac50f3b3887b9bd59c6f43
nguyen trung <trungnguyen14569@gmail.com>
2022-06-19 18:56:23 +0700

    Add project files.

---
+++ Controllers/AttendancesController.cs
@@ -0,0 +1,41 @@
+﻿using LabBigSchool.DTOs;
+using LabBigSchool.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LabBigSchool.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+
+
+        private ApplicationDbContext _dbContext;
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto attendanceDTO)
+        {
+            var userID = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeID == userID && a.CouresID == attendanceDTO.CourseId))
+                return BadRequest("The Attendance already exists!");
+            var attendance = new Attendance
+            {
+                CouresID = attendanceDTO.CourseId,
+                AttendeeID = userID
+            };
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+    }
+
+
+}
---
+++ Controllers/FollowingsController.cs
@@ -0,0 +1,36 @@
+﻿using LabBigSchool.DTOs;
+using LabBigSchool.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace LabBigSchool.Controllers
+{
+    public class FollowingsController : ApiController
+    {
+        private readonly ApplicationDbContext _dbContext;
+    public FollowingsController()
+    {
+        _dbContext = new ApplicationDbContext();
+    }
+    [System.Web.Http.HttpPost]
+    public IHttpActionResult Attend(FollowingDto followingDto)
+    {
+        var userID = User.Identity.GetUserId();
+        if (_dbContext.followings.Any(f => f.FollowerID == userID && f.FolloweeID == followingDto.FolloweeId))
+        return BadRequest("The Attendance already exists!");
+        var following = new Following
+        {
+            FollowerID = userID,
+            FolloweeID = followingDto.FolloweeId
+        };
+        _dbContext.followings.Add(following);
+        _dbContext.SaveChanges();
+        return Ok();
+    }
+}
+}
